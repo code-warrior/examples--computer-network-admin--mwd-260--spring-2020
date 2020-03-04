09e85b605ca1bd04f374173e4d632bdbb0e0220d
Roy A Vanegas <roy@thecodeeducators.com>
2020-03-04 08:47:08 -0500

    Add an if/else clause in order to make the interaction a little more intuitive

--- 04-march-2020/positional-parameters/dollar-sign-asterisk.sh
+++ 04-march-2020/positional-parameters/dollar-sign-asterisk.sh
@@ -3,6 +3,11 @@
 # “This is a single string that consists of all of the positional parameters, separated by the first character in the value of the environment variable IFS (internal field separator), which is a space, TAB, and NEWLINE by default.”
 # — Learning the bash Shell, p86.
 
-echo "$*"
+# If the first argument is null (that is, a length of 0),...
+if [ -z "$1" ]; then
+    echo "No arguments were entered into the command line."
+else
+    echo "$*"
+fi
 
 exit 0
--- 04-march-2020/positional-parameters/dollar-sign-at-sign.sh
+++ 04-march-2020/positional-parameters/dollar-sign-at-sign.sh
@@ -3,6 +3,11 @@
 # “This is equal to “$1” "$2" ... "$N", where N is the number of positional parameters. That is, it’s equal to N separate, double-quoted strings, which are separated by spaces.”
 # — Learning the bash Shell, p86.
 
-echo "$@"
+# If the first argument is null (that is, a length of 0),...
+if [ -z "$1" ]; then
+    echo "No arguments were entered into the command line."
+else
+    echo "$@"
+fi
 
 exit 0
