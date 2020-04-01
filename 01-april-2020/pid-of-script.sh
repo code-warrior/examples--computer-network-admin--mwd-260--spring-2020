805eda673290c1f8558deff3457c6ce3346096ea
Roy A Vanegas <roy@thecodeeducators.com>
2020-04-01 11:21:19 -0400

    Add example of using $$ to retrieve the script’s PID

---
+++ 01-april-2020/pid-of-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Redirect output of this script’s PID ($$) to a new (or exising) file called pid.txt
+echo $$ > pid.txt
+
+# Keep this script running so we can see the PID of the current script
+# using commands such as the “ps” command.
+for (( ; ; )); do
+   echo "" > /dev/null 2>&1
+done
+
+exit 0
